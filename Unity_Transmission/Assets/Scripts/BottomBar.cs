64159f8c2cec7174dc194ddb4c9d94731d02033c
Ivo van der Marel <ivo.marel@gmail.com>
2018-01-27 10:21:48 +0800

    Added some more parameters

---
+++ Unity_Transmission/Assets/Scripts/BottomBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomBar : MonoBehaviour {
+
+    public Text currrentSteerText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currrentSteerText.text = PlayerController.instance.currentSteerRotation.ToString();
+	}
+}
--- Unity_Transmission/Assets/Scripts/PlayerController.cs
+++ Unity_Transmission/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class PlayerController : Singleton<PlayerController> {
 
-    public float speed;
-    public float steeringSpeed;
+    /// <summary>
+    /// The speed increase when fully pushing the controller
+    /// </summary>
+    public float speedIncrease;
+    public float speedAutoDecrease;
+    public float speedBreakDecrease;
+    public float maxBackwardsSpeed;
+    public float maxSpeed;
 
+    internal float currentSpeed;
+
+    [Header("Steering")]
+    [Range(0, 4)]
+    public int steerControlIndex;
+	public float steeringSpeed;
+	public float maxSteer;
+
+    internal float currentSteerRotation;
+
+    //Components
     Rigidbody rb;
 
+    float horizontalInput;
+    float verticalInput;
 
     // Use this for initialization
     void Start () {
@@ -18,13 +37,40 @@ public class PlayerController : Singleton<PlayerController> {
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        horizontalInput  = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        rb.angularVelocity = new Vector3(0, horizontalInput * steeringSpeed *  verticalInput);
-
-        Vector3 rotatedInput = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * new Vector3(0, 0, verticalInput);
-
-        rb.velocity = rotatedInput * speed;
+        Speeding();
+        Steering();
 	}
+
+    void Speeding () {
+        currentSpeed = rb.velocity.magnitude;
+
+        currentSpeed -= speedAutoDecrease * Time.deltaTime;
+
+        //When pressing forward, add gas
+       // if (verticalInput > 0)
+        {
+            currentSpeed += speedIncrease * verticalInput * Time.deltaTime;
+        }
+
+        currentSpeed = Mathf.Clamp(currentSpeed, -maxBackwardsSpeed, maxSpeed);
+
+		Vector3 rotatedInput = Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up) * new Vector3(0, 0, 1);
+
+		//Actually applying to the RB
+        rb.velocity = rotatedInput.normalized * currentSpeed;
+    }
+
+    void Steering()
+    {
+        currentSteerRotation += horizontalInput * steeringSpeed;
+        currentSteerRotation = Mathf.Clamp(currentSteerRotation, -maxSteer, maxSteer);
+
+
+        //Actually applying to the RB
+        rb.angularVelocity = new Vector3(0, currentSteerRotation * currentSpeed);
+    }
+
 }
