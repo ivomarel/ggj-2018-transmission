185d507e3e301ebda16308462b500bac9151e7b9
Ivo van der Marel <ivo.marel@gmail.com>
2018-01-28 09:57:20 +0800

    Added AI state machine

--- Unity_Transmission/Assets/Scripts/PartyGuy.cs
+++ Unity_Transmission/Assets/Scripts/PartyGuy.cs
@@ -7,21 +7,76 @@ using Pathfinding.Examples;
 
 public class PartyGuy : MonoBehaviour
 {
-
-
+	
+	public float conversionTime;
 	public float hitVelocityToDie;
 	public float speed;
 	public float rotationSpeed;
 	public float stoppingDistance;
 	public Blood bloodPrefab;
 
+	public enum State
+	{
+		Trumping,
+		BeingConverted,
+		Trancing
+	}
+
+	internal State currentState;
+
 	float realSpeed;
 
 	private Rigidbody rb;
+	private SpriteRenderer sr;
 	// Use this for initialization
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();	
+		sr = GetComponent<SpriteRenderer> ();
+		SetState (State.Trumping);
+	}
+
+	public void SetState (State newState)
+	{
+		StopAllCoroutines ();
+		currentState = newState;
+		switch (currentState) {
+		case State.Trumping:
+			StartCoroutine (OnTrumping ());
+			break;
+		case State.BeingConverted:
+			StartCoroutine (OnConverting ());
+			break;
+		case State.Trancing:
+			StartCoroutine (OnTrancing ());
+			break;
+		}
+	}
+
+	IEnumerator OnTrumping ()
+	{
+		while (true) {
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+
+	IEnumerator OnConverting ()
+	{
+		float timer = 0;
+		while (timer < conversionTime) {
+			timer += Time.fixedDeltaTime;
+			sr.color = Color.Lerp (Color.white, Color.red, timer / conversionTime);
+			yield return new WaitForFixedUpdate ();
+		}
+
+		SetState (State.Trancing);
+	}
+
+	IEnumerator OnTrancing ()
+	{
+		while (true) {
+			yield return new WaitForFixedUpdate ();
+		}
 	}
 	
 	// Update is called once per frame
