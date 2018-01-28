1f9dd160a392a20b483137695938130cc88e8376
Ivo van der Marel <ivo.marel@gmail.com>
2018-01-28 11:02:46 +0800

    Added conversion

---
+++ Unity_Transmission/Assets/Scripts/ConversionRadar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionRadar : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider otherCollider)
+	{
+		PartyGuy guy = otherCollider.GetComponent<PartyGuy> ();
+		if (guy != null && guy.currentState == PartyGuy.State.Trumping) {
+			guy.SetState (PartyGuy.State.BeingConverted);
+		}
+	}
+
+	void OnTriggerExit (Collider otherCollider)
+	{
+		PartyGuy guy = otherCollider.GetComponent<PartyGuy> ();
+		if (guy != null) {
+			//When he is not fully converted yet, go back to Trumping
+			if (guy.currentState == PartyGuy.State.BeingConverted) {
+				guy.SetState (PartyGuy.State.Trumping);
+			}
+		}
+	}
+}
